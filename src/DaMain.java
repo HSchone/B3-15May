2f369056ba252cf10d1b9993fe0dbf63c14c2996
HSchone <pizzazzy7@gmail.com>
2017-05-15 12:50:32 -0600

    Fixed the reset error

--- src/DaMain.java
+++ src/DaMain.java
@@ -3,8 +3,15 @@ public class DaMain {
 
 	public static void main(String[] args) {
 		System.out.println("Initial build");
+
 		System.out.println("Working on a bug fix");
 		System.out.println("Yay! The bug if fixed!");
+
+		
+		
+		System.out.println("Good luck! I am trying a totally new GUI");
+		System.out.println("Whew! It works!!!");
+
 
 	}
 
