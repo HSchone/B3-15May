e215a637af99f5a5f1d79a62c66974e893c53024
HSchone <pizzazzy7@gmail.com>
2017-05-15 12:28:23 -0600

    Bug Fixed

--- src/DaMain.java
+++ src/DaMain.java
@@ -3,6 +3,8 @@ public class DaMain {
 
 	public static void main(String[] args) {
 		System.out.println("Initial build");
+		System.out.println("Working on a bug fix");
+		System.out.println("Yay! The bug if fixed!");
 
 	}
 
